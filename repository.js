e2a91ab05465b0f9e8b0e3d6a93a3faab7610229
nathan <nathan@fjmertz.com>
2022-08-11 07:21:14 +0000

    demo modification with cicd

--- repository.js
+++ repository.js
@@ -29,7 +29,7 @@ const mockEvents = {
     events: [
         { id: 1 , title: 'a mock event', description: 'something really cool', location: 'Chez Joe Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
         { id: 2, title: 'another mock event', description: 'something even cooler', location: 'Chez John Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
-        // { id: 3, title: 'automated mock event', description: 'automation is awesoem', location: 'Chez DevOps Deployment', likes: 0, datetime_added: '2022-02-01:12:00' },
+        { id: 3, title: 'automated mock event', description: 'automation is awesoem', location: 'Chez DevOps Deployment', likes: 0, datetime_added: '2022-02-01:12:00' },
     ]
 };
 
